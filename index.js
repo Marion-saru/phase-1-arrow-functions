7e1c6c4c14c7bbb2f472027ae357771e9ce129c5
Marion Saru <maghangasaru@gmail.com>
2025-04-25 22:20:00 +0300

    Completed optional lab

---
+++ index.js
@@ -0,0 +1,16 @@
+let a = 2000;
+let b = 100;
+
+let divide = function(a, b){
+    return a/b;
+}
+
+console.log(divide(a, b));
+
+let square = c => c **2;
+console.log(square(5));
+
+let add = (d, e) => {
+    return d + e;
+}
+console.log(add(3,5));
